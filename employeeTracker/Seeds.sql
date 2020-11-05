be57b42603b4210e8ee3ab8116b84baefb111d18
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-11-04 22:03:34 -0700

    Added details about employees and departments to Seeds.sql

--- employeeTracker/Seeds.sql
+++ employeeTracker/Seeds.sql
@@ -26,3 +26,12 @@ role_id INT NOT NULL,
 manager_id INT,
 PRIMARY KEY (id)
 );
+
+INSERT INTO departments (name)
+VALUES("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES("Sales_Lead", 100000, 1), ("Lead_Engineer", 150000, 2), ("Accountant", 125000, 3), ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES("John", "Doe", 1), ("Ashley", "Rodriguez", 2), ("Malia", "Brown", 3), ("Tom", "Allen", 4);
